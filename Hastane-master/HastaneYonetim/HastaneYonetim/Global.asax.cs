9307be5cf0ff095793f4dd9d9f4f70fb9a99826a
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2020-06-13 19:21:35 +0300

    düzgün

--- Hastane-master/HastaneYonetim/HastaneYonetim/Global.asax.cs
+++ Hastane-master/HastaneYonetim/HastaneYonetim/Global.asax.cs
@@ -7,6 +7,8 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using AutoMapper;
+using System.Web.Optimization;
 
 namespace HastaneYonetim
 {
@@ -15,9 +17,12 @@ namespace HastaneYonetim
         void Application_Start(object sender, EventArgs e)
         {
             // Uygulama başlangıcında çalışan kod
+            Mapper.Initialize(c => c.AddProfile<App_Start.HaritalamaProfili>());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Kayit);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
